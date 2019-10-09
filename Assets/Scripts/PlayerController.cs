6e4ffea215c6d5acafd6c3fbf523f3a2e0f75d48
012345678927 <mcdonald.matthew.m@hotmail.com>
2019-10-09 13:51:59 -0400

    Committed  Initial Unity Files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // Fixed update is used when manipulating physics
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float verti = Input.GetAxis("Vertical");
+
+        // Modify player velocity through rigidbody2d
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = new Vector2(horiz * speed, verti * speed);
+    }
+}
